42b5de0437b1978e50167b1a825b821a7145b86b
lihereshma <reshmalihe00068@gmail.com>
2020-09-03 08:46:42 +0530

    Updates

---
+++ precache-manifest.14563ae36059c1b9f593177fa178cb40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7f2ae54220dde550d3d76fd1c299b4",
+    "url": "/moviedb-react/index.html"
+  },
+  {
+    "revision": "4e8e568f8fcab85b1606",
+    "url": "/moviedb-react/static/css/main.c6986438.chunk.css"
+  },
+  {
+    "revision": "df037f10569c42fb9300",
+    "url": "/moviedb-react/static/js/2.e292bf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb-react/static/js/2.e292bf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8e568f8fcab85b1606",
+    "url": "/moviedb-react/static/js/main.6e68096b.chunk.js"
+  },
+  {
+    "revision": "7c92fc800065506d0274",
+    "url": "/moviedb-react/static/js/runtime-main.dc2d3794.js"
+  }
+]);
